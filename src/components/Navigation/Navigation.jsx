c81979f48e4596f40b827951506a463b9f5f6685
Blaž <blaz.bocko1@gmail.com>
2023-04-28 22:43:56 +0200

    Add tailwind styling

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Navigation = () => {
     return (
         <nav className="flex justify-end">
-            <p>Sign Out</p>
+            <p className="underline text-blue-200 text-lg cursor-pointer transition-opacity duration-200 hover:opacity-80 active:opacity-60">Sign Out</p>
         </nav>
     );
 }
