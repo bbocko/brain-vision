e365ff77ff37728791abf546c83f8376e262aeda
Blaž <blaz.bocko1@gmail.com>
2023-04-30 17:46:24 +0200

    Add few styles for button

--- src/components/ImageLinkForm/ImageLinkForm.jsx
+++ src/components/ImageLinkForm/ImageLinkForm.jsx
@@ -2,13 +2,13 @@ import React from "react";
 
 const ImageLinkForm = () => {
     return (
-        <div className="flex flex-col items-center mt-20">
+        <div className="flex flex-col items-center mt-12">
             <p className="text-blue-300 text-lg text-center">
                 This magic brain will detect faces in your pictures
             </p>
             <div className="flex flex-col mt-4 sm:flex-row sm:justify-center">
                 <input className="block mb-2 sm:mr-2 sm:mb-0 px-4 py-2 rounded-lg text-gray-800 w-full sm:w-72" type="text" placeholder="Picture link" />
-                <button className="px-4 py-2 rounded-lg text-white bg-blue-500 hover:bg-blue-600 w-full sm:w-auto">Detect</button>
+                <button className="px-4 py-2 rounded-lg text-white bg-blue-500 hover:bg-blue-600 transition duration-200 active:opacity-80 w-full sm:w-auto">Detect</button>
             </div>
         </div>
     );
