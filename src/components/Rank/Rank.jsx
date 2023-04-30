971dce1a8565579e07cf19d6692f1750d3370d96
Blaž <blaz.bocko1@gmail.com>
2023-04-30 17:46:28 +0200

    Create Rank.jsx

---
+++ src/components/Rank/Rank.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Rank = () => {
+    return (
+        <div className="mt-10">
+            <div className="text-blue-300 text-xl text-center">
+                {`User, your rank is:`}
+            </div>
+            <div className="text-blue-300 text-3xl text-center mt-2">
+                {`#5`}
+            </div>
+        </div>
+    );
+}
+
+export default Rank;
