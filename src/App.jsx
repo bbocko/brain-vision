b21c4fc6cfc4599e91e602fa22190fed61149d58
Blaž <blaz.bocko1@gmail.com>
2023-04-30 17:46:50 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,24 @@
-import React from 'react'
-import './App.css'
-import Navigation from './components/Navigation/Navigation'
+import React from "react";
+import { useMediaQuery } from 'react-responsive';
+import "./App.css";
+import Navigation from "./components/Navigation/Navigation";
+import Logo from "./components/Logo/Logo";
+import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
+import Rank from "./components/Rank/Rank";
+import ParticlesBg from 'particles-bg';
 
 function App() {
+  const isDesktop = useMediaQuery({ minDeviceWidth: 1024 });
+  const numParticles = isDesktop ? 50 : 10;
 
   return (
     <div className="App">
+      <ParticlesBg color="#FFFFFF" num={numParticles} type="cobweb" bg={true} />
       <Navigation />
-      {/* <Logo />
+      <Logo />
+      <Rank />
       <ImageLinkForm />
-      <FaceRecognition /> */}
+      {/* <FaceRecognition /> */}
     </div>
   )
 }
