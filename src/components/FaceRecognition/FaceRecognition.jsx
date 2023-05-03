030375378f7f4af90507855f3a5610ecc83f9dc9
Blaž <blaz.bocko1@gmail.com>
2023-05-03 18:14:50 +0200

    Update FaceRecognition.jsx

--- src/components/FaceRecognition/FaceRecognition.jsx
+++ src/components/FaceRecognition/FaceRecognition.jsx
@@ -1,10 +1,15 @@
 import React from "react";
+import "./FaceRecognition.css"
 
-const FaceRecognition = ({ imageURL }) => {
+const FaceRecognition = ({ imageURL, box }) => {
     return (
-        <div className="flex justify-center mt-5">
-            <img className="max-w-[500px] max-h-[500px]"
-                src={imageURL} alt="" />
+        <div className="flex justify-center mt-8">
+            <div className="relative">
+                <img id="inputImage" className="max-w-[600px] max-h-[600px]"
+                    src={imageURL} alt="" />
+                <div className="bounding-box absolute"
+                    style={{ top: box.topRow, right: box.rightCol, bottom: box.bottomRow, left: box.leftCol }}></div>
+            </div>
         </div>
     );
 }
