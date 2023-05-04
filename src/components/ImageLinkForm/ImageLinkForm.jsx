70641d22410b14aef2b1ee8a77415fc81cb3c096
Blaž <blaz.bocko1@gmail.com>
2023-05-05 00:08:55 +0200

    Update text color

--- src/components/ImageLinkForm/ImageLinkForm.jsx
+++ src/components/ImageLinkForm/ImageLinkForm.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const ImageLinkForm = ({ onInputChange, onButtonSubmit }) => {
     return (
         <div className="flex flex-col items-center mt-10">
-            <p className="text-blue-300 text-lg text-center">
+            <p className="text-blue-200 text-lg text-center">
                 This magic brain will detect faces in your pictures
             </p>
             <div className="flex flex-col mt-4 sm:flex-row sm:justify-center">
@@ -13,7 +13,7 @@ const ImageLinkForm = ({ onInputChange, onButtonSubmit }) => {
                     placeholder="Picture link"
                     onChange={onInputChange} />
                 <button
-                    className="px-4 py-2 rounded-lg text-white bg-blue-500 hover:bg-blue-600 transition duration-200 active:opacity-80 w-full sm:w-auto"
+                    className="px-4 py-2 rounded-lg text-white bg-blue-500 hover:bg-blue-600 transition duration-150 active:opacity-80 w-full sm:w-auto"
                     onClick={onButtonSubmit}>
                     Detect
                 </button>
--- src/components/Rank/Rank.jsx
+++ src/components/Rank/Rank.jsx
@@ -3,10 +3,10 @@ import React from "react";
 const Rank = () => {
     return (
         <div className="mt-10">
-            <div className="text-blue-300 text-xl text-center">
+            <div className="text-blue-200 text-xl text-center">
                 {`User, your rank is:`}
             </div>
-            <div className="text-blue-300 text-3xl text-center mt-2">
+            <div className="text-blue-200 text-3xl text-center mt-2">
                 {`#5`}
             </div>
         </div>
