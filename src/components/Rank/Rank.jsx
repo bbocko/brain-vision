ad0862fd23837478814aad23ec46ef89ee3b39b1
Blaž <blaz.bocko1@gmail.com>
2023-05-29 21:48:27 +0200

    Update Rank.jsx

--- src/components/Rank/Rank.jsx
+++ src/components/Rank/Rank.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 
-const Rank = () => {
+const Rank = ({ name, entries }) => {
     return (
         <div className="my-10">
             <div className="text-blue-200 text-xl text-center">
-                {`User, your rank is: `}
+                {`${name}, your current entry count is: `}
                 <span className="text-blue-200 text-2xl">
-                    {`#5`}
+                    {entries}
                 </span>
             </div>
         </div>
