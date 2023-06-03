f947840297292d697f464c69a8a0557f8a240d91
Blaž <blaz.bocko1@gmail.com>
2023-06-03 11:34:43 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import "./App.css";
 import Navigation from "./components/Navigation/Navigation";
 import Logo from "./components/Logo/Logo";
